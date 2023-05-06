e6395e5b15690a3c3b7da9cf2ffab63312a93de9
user <venkateshchennaa@gmail.com>
2023-05-06 23:26:27 +0530

    first commit

--- src/test/java/BasicSeleniumPrograms/hi_bi.java
+++ src/test/java/BasicSeleniumPrograms/hi_bi.java
@@ -1,5 +1,26 @@
 package BasicSeleniumPrograms;
 
-public class hi_bi {
+import java.util.Scanner;
 
+public class hi_bi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide number");
+        int n = sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+    if (i%3==0 &&i%5 ==0 ){
+        System.out.println("Hi-Bi");
+    }
+    else if(i%3 ==0) {
+        System.out.println("Hi");
+    }
+    else if(i%5 ==0) {
+        System.out.println("Bi");
+    }
+    else  {
+    System.out.println(i);
+}
+}
+    }
 }
